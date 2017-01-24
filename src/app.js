a5ba74c21c0a97e869e75ccf336a6db68a1ecfae
Stanislas Bernard <bernardstanislas@gmail.com>
2017-01-24 11:43:09 +0100

    Working communication channels

--- src/app.js
+++ src/app.js
@@ -10,117 +10,113 @@ const webSockerURI = `ws://localhost:${SERVER_PORT}/`;
 const webSocket = new WebSocket(webSockerURI);
 
 const CLIENT_ID = uuid.v4();
+document.title = CLIENT_ID;
 
 webSocket.onopen = () => {
   webSocket.send(JSON.stringify({
     type: 'HELLO',
-    destination: CLIENT_ID 
+    source: CLIENT_ID
   }));
 };
 
 webSocket.onmessage = rawMessage => {
   const message = JSON.parse(rawMessage.data);
-  console.log(message);
-  switch(message.type) {
-    case 'HELLO': {
-      const {destination} = message;
-      if (destination !== CLIENT_ID) {
-        console.log('Will create a peer connection');
+  if (message.source !== CLIENT_ID && message.type === 'HELLO') {
+    const peerConnection = new rtc.RTCPeerConnection(iceServers, {
+      optional: [{
+        DtlsSrtpKeyAgreement: true
+      }]
+    });
+
+    const channel = peerConnection.createDataChannel('communication', {
+      reliable: false
+    });
+
+    channel.onmessage = ({data}) => {
+      console.log('Received data from channel', message.source, data);
+    };
+
+    channel.onopen = () => {
+      peerChannelsStore.addPeerChannel(message.source, channel);
+      console.log(`Created communication channel with ${message.source}`);
+    };
+
+    peerConnection.createOffer(offer => {
+      peerConnection.setLocalDescription(offer);
+      webSocket.send(JSON.stringify({
+        type: 'OFFER',
+        source: CLIENT_ID,
+        destination: message.source,
+        payload: offer
+      }));
+    }, console.error.bind(console));
+
+    peerConnection.onicecandidate = ({candidate}) => {
+      if (candidate) {
+        webSocket.send(JSON.stringify({
+          type: 'ICE_CANDIDATE',
+          source: CLIENT_ID,
+          destination: message.source,
+          payload: candidate
+        }));
+      }
+    };
+
+    peerConnectionsStore.addPeerConnection(message.source, peerConnection);
+  }
+  if (message.destination === CLIENT_ID) {
+    console.log(message);
+    switch(message.type) {
+      case 'ICE_CANDIDATE': {
+        const peerConnection = peerConnectionsStore.peerConnections[message.source];
+        peerConnection.addIceCandidate(new rtc.RTCIceCandidate(message.payload));
+        break;
+      }
+      case 'OFFER': {
         const peerConnection = new rtc.RTCPeerConnection(iceServers, {
           optional: [{
             DtlsSrtpKeyAgreement: true
           }]
         });
 
-        const channel = peerConnection.createDataChannel('communication', {
-          reliable: false
-        });
-
-        channel.onmessage = ({data}) => {
-          console.log('Received data from channel', destination, data);
-        };
-
-        channel.onopen = () => {
-          peerChannelsStore.addPeerChannel(destination, channel);
-          console.log(`Created communication channel with ${destination}`);
-        };
-
-        peerConnection.createOffer(offer => {
-          peerConnection.setLocalDescription(offer);
-          webSocket.send(JSON.stringify({
-            type: 'OFFER',
-            source: CLIENT_ID,
-            destination,
-            payload: offer
-          }));
-          console.log(`Sent offer to ${destination}`);
-        }, console.error.bind(console));
+        peerConnectionsStore.addPeerConnection(message.source, peerConnection);
 
         peerConnection.onicecandidate = ({candidate}) => {
-          console.log('Ice candidate !!');
           if (candidate) {
             webSocket.send(JSON.stringify({
               type: 'ICE_CANDIDATE',
               source: CLIENT_ID,
-              destination: destination,
+              destination: message.source,
               payload: candidate
             }));
           }
         };
 
-        peerConnectionsStore.addPeerConnection(destination, peerConnection);
-      }
-      break;
-    }
-    case 'ICE_CANDIDATE': {
-      const peerConnection = peerConnectionsStore.peerConnections[message.source];
-      peerConnection.addIceCandidate(new rtc.RTCIceCandidate(message.payload));
-      break;
-    }
-    case 'OFFER': {
-      const peerConnection = new rtc.RTCPeerConnection(iceServers, {
-        optional: [{
-          DtlsSrtpKeyAgreement: true
-        }]
-      });
-
-      peerConnectionsStore.addPeerConnection(message.source, peerConnection);
-
-      peerConnection.onicecandidate = ({candidate}) => {
-        if (candidate) {
-          webSocket.send(JSON.stringify({
-            type: 'ICE_CANDIDATE',
-            source: CLIENT_ID,
-            destination: destination,
-            payload: candidate
-          }));
-        }
-      };
-
-      peerConnection.ondatachannel = ({channel}) => {
-        peerChannelsStore.addPeerChannel(source, channel);
-        channel.onmessage = ({data}) => {
-          console.log('Received data from channel', source, data);
+        peerConnection.ondatachannel = ({channel}) => {
+          peerChannelsStore.addPeerChannel(message.source, channel);
+          channel.onmessage = ({data}) => {
+            console.log('Received data from channel', message.source, data);
+          };
+          console.log(`Received communication channel from ${message.source}`);
         };
-        console.log(`Received communication channel from ${source}`);
-      };
 
-      peerConnection.setRemoteDescription(new rtc.RTCSessionDescription(message.payload));
-      peerConnection.createAnswer(answer => {
-        peerConnection.setLocalDescription(answer);
-        webSocket.send(JSON.stringify({
-          type: 'ANSWER',
-          source: CLIENT_ID,
-          destination: message.source,
-          payload: answer
-        }));
-      });
-      break;
-    }
-    case 'ANSWER': {
-      const peerConnection = peerConnectionsStore.peerConnections[message.source];
-      peerConnection.setRemoteDescription(new rtc.RTCSessionDescription(message.payload));
-      break;
+        peerConnection.setRemoteDescription(new rtc.RTCSessionDescription(message.payload));
+        peerConnection.createAnswer(answer => {
+          peerConnection.setLocalDescription(answer);
+          webSocket.send(JSON.stringify({
+            type: 'ANSWER',
+            source: CLIENT_ID,
+            destination: message.source,
+            payload: answer
+          }));
+        }, e => console.log('Answer error', e));
+        break;
+      }
+      case 'ANSWER': {
+        const peerConnection = peerConnectionsStore.peerConnections[message.source];
+        peerConnection.setRemoteDescription(new rtc.RTCSessionDescription(message.payload));
+        break;
+      }
     }
   }
 }
